d9bb79c35e45b0cf37d41d4cd0673da3c87cdfb4
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-03-01 09:55:12 +0200

    Initial commit

---
+++ src/main/java/com/collectionsone/collectionsone/CollectionsoneApplication.java
@@ -0,0 +1,13 @@
+package com.collectionsone.collectionsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsoneApplication.class, args);
+	}
+
+}
